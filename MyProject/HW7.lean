4ffb3488d72873162b37c080522acfd45912a57c
Sam Weston <skweston123@gmail.com>
2025-02-24 13:01:56 -0800

    added hw 7

---
+++ MyProject/HW7.lean
@@ -0,0 +1,12 @@
+/-
+
+Prepare a project update for Thursday Feb 27.
+
+For the presentation, please pick one concept, method, or idea you would like to share, discuss or get feedback on.
+
+For your projects, I suggest the following:
+  - Focus on making your code beautiful and the writing around it look like a textbook.
+  - Keep the meet of your proofs of your theorems to 5-15 lines long. If they get longer, find ways to break out theorems, add methods to the simplifier, and so on. Consider making your own library of such helper theorems, and try to express them generally, so they are reusable.
+  - Focus on the types you are using. Introduce new structures and/or inductive types when needed and build the theorems that support those types.
+
+-/
