4b880a56c65798c719baaf4cd8ce6c1f06f78413
Sam Weston <skweston123@gmail.com>
2025-03-11 12:50:30 -0700

    things and stuff

--- MyProject/Libraries/ControlLib.lean
+++ MyProject/Libraries/ControlLib.lean
@@ -29,6 +29,24 @@ def ctrbMat {n : ℕ}
   listToMat (find_ctrb A B)
 
 
+def find_eqb {n : ℕ}
+(e : α)
+(q : (Mat 1 n))
+(B : (Mat n 1))
+(i : ℕ := n)
+: List (Mat 1 1) :=
+  if i-1 = 0
+    then [q*B]
+  else
+    (find_eqb e q B (i-1)) ++ [e^(i-1)•q*B]
+
+def eqbMat {n : ℕ}
+(e : α)
+(q : (Mat 1 n))
+(B : (Mat n 1)) :=
+  listToMat (find_eqb e q B)
+
+
 -- Constructs the matrix [(A-λI) B]
 def ABe {n : ℕ}
 (A : Mat n n)
--- MyProject/Proofs/three_iff_four.lean
+++ MyProject/Proofs/three_iff_four.lean
@@ -1,5 +1,6 @@
 import Mathlib.Tactic
 import MyProject
+import Mathlib.Data.Matrix.Basic
 
 
 theorem three_to_four :
@@ -8,30 +9,42 @@ is_full_rank (ctrbMat A B) →
 ∀ (e : α), is_eig_val A e →
 is_full_rank (ABe A B e) := by
   unfold is_full_rank
-  intro A B hq e hev q qnz
-  have ctrbFR := hq q qnz
-  unfold ctrbMat listToMat at ctrbFR
+  intro A B hq e hev q qNZ
+  have ctrbFR := hq q qNZ
+  --unfold ctrbMat listToMat at ctrbFR
   unfold ABe
   by_contra ABeFR
-  have qBz : q*B=0 := by
+  have qBZ : q*B=0 := by
     sorry
   have qAe : q*A=e•q := by
     --apply distrib_ofBlocks q A B
     sorry
-  have qAek : q*(A^n)=(e^n)•q := by
-    sorry
+  have qAek : ∀ (k : ℕ), q*(A^k)=(e^k)•q := by
+    intro k
+    induction k with
+    | zero => simp
+    | succ k' ih =>
+      sorry
+  obtain ctrbNFR : q*ctrbMat A B = 0 := by
+    -- unfold ctrbMat listToMat
+    -- ext i j
+    -- rcases i
+    -- rcases j
+    -- rename_i i hi j ji
+    induction n with
+    | zero => unfold ctrbMat listToMat; sorry
+    | succ n' ih =>
+      sorry
+  exact ctrbFR ctrbNFR
 
 
 
-
-
-
-
-
-
+theorem four_to_three :
+∀ (A : Mat n n) (B : Mat n 1) (e : α),
+is_eig_val A e →
+is_full_rank (ABe A B e) →
+is_full_rank (ctrbMat A B) := by
   sorry
-
-
 
 theorem three_iff_four :
 ∀ (A : Mat n n) (B : Mat n 1) (e : ℚ),
@@ -39,6 +52,5 @@ theorem three_iff_four :
 ((is_full_rank (ctrbMat A B) ↔ is_full_rank (ABe A B e))) := by
   intro A B e he
   constructor
-  .
-    sorry
+  . sorry
   . sorry
